0c07bf76684942a0b54724c6684ba73e88bf5e1f
Alex Peattie <alexpeattie@gmail.com>
2017-02-15 16:32:44 +0000

    Typo fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ controller.hears(['subscribe to ([a-z0-9_\\-,\\s]+)'], 'direct_message,direct_me
       if (!error && subResponse.statusCode == 200) {
         bot.reply(message, `Good news, that's all set up for you :thumbsup:. You'll get a notification in this channel whenever ${ channelId } publishes a new video!`)
       } else if(subResponse.statusCode == 404) {
-        bot.reply(message, "Hmm it looks like I ran into a problem finding that channel. Is it possible you mispell the name :speak_no_evil:?")
+        bot.reply(message, "Hmm it looks like I ran into a problem finding that channel. Is it possible you mispelled the name :speak_no_evil:?")
       } else {
         bot.reply(message, "Sorry, I couldn't set up your subscription :cry:! You can try again later, or contact your administrator if the problem persists.")
       }
@@ -31,6 +31,8 @@ controller.hears(['subscribe to ([a-z0-9_\\-,\\s]+)'], 'direct_message,direct_me
 })
 
 controller.hears(['unsubscribe from ([a-z0-9_\\-,\\s]+)'], 'direct_message,direct_mention,mention', (bot, message) => {
+  var channelId = message.match[1]
+
   bot.reply(message, `Ok let me remove any subscriptions to ${ channelId }...`)
 
   let unsubUrl = `https://y026iupl4i.execute-api.us-east-1.amazonaws.com/latest/subscription?id_or_username=${ channelId }&slack_channel_id=${ message.channel }&unsubscribe=true`
