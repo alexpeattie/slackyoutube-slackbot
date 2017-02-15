2aa9c00e279010cab5571a8851f07d44ae8b541d
Alex Peattie <alexpeattie@gmail.com>
2017-02-15 15:10:18 +0000

    Add slack channel id, GET -> POST

--- app.js
+++ app.js
@@ -12,14 +12,13 @@ const bot = controller.spawn({
 
 controller.hears(['subscribe to ([a-z0-9_\\-,\\s]+)'], 'direct_message,direct_mention,mention', (bot, message) => {
   var channelId = message.match[1]
-  console.log(message.channel)
 
   bot.api.users.info({ user: message.user }, (error, response) => {
     let { name, real_name } = response.user;
     bot.reply(message, `Ok <@${ name }>, I'll try and set up a subscription to ${ channelId }...`)
 
-    let newSubUrl = `https://y026iupl4i.execute-api.us-east-1.amazonaws.com/latest/subscription?channel=${ channelId }&slack_cid=${ message.channel }`
-    request(newSubUrl, (error, subResponse) => {
+    let newSubUrl = `https://y026iupl4i.execute-api.us-east-1.amazonaws.com/latest/subscription?id_or_username=${ channelId }&slack_channel_id=${ message.channel }`
+    request.post(newSubUrl, (error, subResponse) => {
       if (!error && subResponse.statusCode == 200) {
         bot.reply(message, `Good news, that's all set up for you :thumbsup:. You'll get a notification in this channel whenever ${ channelId } publishes a new video!`)
       } else {
@@ -32,13 +31,12 @@ controller.hears(['subscribe to ([a-z0-9_\\-,\\s]+)'], 'direct_message,direct_me
 controller.setupWebserver(PORT, (error, webserver) => {
   webserver.post('/video', (req, res) => {
     bot.api.channels.list({}, (error, channelResponse) => {
-      let channel = channelResponse.channels.find(c => c.name == 'general').id
-      const missingKeys = ['username', 'title', 'id'].filter(k => !req.body[k])
+      const missingKeys = ['username', 'title', 'id', 'slack_channel_id'].filter(k => !req.body[k])
 
       if(missingKeys.length === 0) {
         bot.say({
           text: `${ req.body.username } just published a new video - *"${ req.body.title }"* (https://www.youtube.com/watch?v=${ req.body.id })`,
-          channel
+          channel: req.body.slack_channel_id
         }, () => { res.status(200).json({ code: "notification_sent", details: "Video notification sent" }) },
            () => { res.status(500).json({ code: "notification_failed", details: "Unexpected server error occured" })
         })
